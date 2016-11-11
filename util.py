bf67d7f751ba8ffc3656b4d3a6a6da202ad2cea6
Jakub Turski <yacoob@gmail.com>
2016-11-11 23:18:14 +0000

    Load fonts from res dir.

--- util.py
+++ util.py
@@ -16,6 +16,14 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+import os
+
+
+RESDIR = 'res'
+CFG = {
+        'fontdir': os.path.join(RESDIR, 'fonts')
+}
+
 def map_from(x, min, max):
     return (x-min) / (max-min)
 
