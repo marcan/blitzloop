8dd3703a39daccd1cf79c377ff52a810d289b357
Jakub Turski <yacoob@gmail.com>
2016-11-16 20:02:59 +0000

    Adjust run.sh to launch installed version.

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
-export PYTHONPATH=src/build/lib.linux-x86_64-2.7/
 while xdpyinfo ; do
-	python -u src/main.py -fs ../songs 1024 768 2>&1 | tee -a /tmp/blitzloop-log.txt
+	blitzloop -fs ../songs 1024 768 2>&1 | tee -a /tmp/blitzloop-log.txt
 	sleep 1
 done
