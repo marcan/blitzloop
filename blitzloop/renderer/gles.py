6c329a58e590a13014aa4014e3786009740560cf
Hector Martin <marcan@marcan.st>
2017-01-18 01:25:27 +0900

    gles: s/GL_CLAMP/GL_CLAMP_TO_EDGE/

--- blitzloop/renderer/gles.py
+++ blitzloop/renderer/gles.py
@@ -411,8 +411,8 @@ class ImageTexture(object):
         gl.glBindTexture(gl.GL_TEXTURE_2D, self.texid)
         gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR_MIPMAP_LINEAR)
         gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
-        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
 
         try:
             blob = self.teximage.tobytes()
