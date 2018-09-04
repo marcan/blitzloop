61785c4fbd9e93573a4badad28de6c967814c0a6
Hector Martin <marcan@marcan.st>
2018-09-05 02:36:36 +0900

    backend.glfw: add missing import

--- blitzloop/backend/glfw.py
+++ blitzloop/backend/glfw.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-import platform
+import os, platform
 
 from blitzloop import util
 import blitzloop.backend.gles_fixes
